ca92a7667a6a15b793e1477361bc9e3d4a26f063
Adam Sobotka <vorcigernix@gmail.com>
2022-09-14 13:31:25 +0200

    Update _app.js
    this is how it works, you need to create the components though (the navfooter.js must export two functions named in import)

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import {React, Component} from 'react'
+import {Navigation, Footer} from '../components/navfooter.js'
 
 function MyApp({ Component, pageProps }) {
-  return <
-    
-  Component {...pageProps} />
+  return (
+    <Navigation />
+    <Component {...pageProps} />
+    <Footer />
+)
 }
 
 export default MyApp

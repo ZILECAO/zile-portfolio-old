64484a8a7e9e57d67e642238a7111fc29f4c4142
ZILECAO <zilecao@sas.upenn.edu>
2022-10-08 23:46:12 -0400

    added video demo link for w3b hackathon

--- pages/projects.js
+++ pages/projects.js
@@ -126,7 +126,7 @@ export default function Projects() {
         <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           <a
             class="block p-8 backdrop-brightness-75 border border-gray-800 shadow-xl transition rounded-xl hover:shadow-blue-500/10 hover:border-blue-500/10"
-            href="https://github.com/ZILECAO/w3b-hackathon"
+            href="https://youtu.be/qpnThqLB7Gk"
           >
             <svg class="h-8 w-8 text-red-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="13" r="7" />  <polyline points="12 10 12 13 14 13" />  <line x1="7" y1="4" x2="4.25" y2="6" />  <line x1="17" y1="4" x2="19.75" y2="6" /></svg>
 
